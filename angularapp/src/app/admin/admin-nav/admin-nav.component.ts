78227d445954d44b887825d38d343dae42e12062
Parvathi31 <meletijayasriparvathi@gmail.com>
2022-04-02 07:13:03 +0000

    admin

---
+++ angularapp/src/app/admin/admin-nav/admin-nav.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-nav',
+  templateUrl: './admin-nav.component.html',
+  styleUrls: ['./admin-nav.component.css']
+})
+export class AdminNavComponent implements OnInit {
+
+  constructor() { 
+    //this is constructor
+  }
+
+  ngOnInit(): void {
+    //this is ngOnInit method
+}
+
+}
